966e6d1572c02feff0a21b6a1e5e0ddde6578116
sergio hilgert <hilgertfacul@gmail.com>
2017-04-01 18:20:21 -0300

    Adiciona spring test

--- src/test/java/br/fatec/atividade07/AppTest.java
+++ src/test/java/br/fatec/atividade07/AppTest.java
@@ -5,10 +5,11 @@ import static org.junit.Assert.assertEquals;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.junit.Before;
 import org.junit.Test;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import br.fatec.atividade07.model.Artigo;
 import br.fatec.atividade07.model.Cartao;
@@ -18,43 +19,46 @@ import br.fatec.atividade07.service.CartaoService;
 import br.fatec.atividade07.service.CategoriaService;
 import br.fatec.atividade07.service.UsuarioService;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("/applicationContext.xml")
 public class AppTest {
 
+	@Autowired
 	private ArtigoService artigoService;
-	private CategoriaService categoriaService;
-	private UsuarioService usuarioService;
-	private CartaoService cartaoService;
 
-	@Before
-	public void setup() {
-		ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-		artigoService = (ArtigoService) context.getBean("artigoService");
-		categoriaService = (CategoriaService) context.getBean("categoriaService");
-		usuarioService = (UsuarioService) context.getBean("usuarioService");
-		cartaoService = (CartaoService) context.getBean("cartaoService");
-	}
+	@Autowired
+	private CategoriaService categoriaService;
+
+	@Autowired
+	private UsuarioService usuarioService;
+
+	@Autowired
+	private CartaoService cartaoService;
 
 	@Test
 	public void test() {
 
 		// Cria cartao
-		Long number = 14L;
+		Long number = 15L;
 		Cartao cartao = new Cartao();
 		cartao.setNumero(number);
 		cartaoService.createCartao(cartao);
+
 		cartao = cartaoService.findByNumero(number);
+
 		assertEquals(number, cartao.getNumero());
 
 		// Cria Usuario
-		String nome = "Sergio05";
+		String nome = "Sergio06";
 		Usuario user = new Usuario();
 		user.setCartao(cartao);
 		user.setNome(nome);
 		usuarioService.criaUsuario(user);
+
 		user = usuarioService.buscaPorNome(nome);
-		
+
 		assertEquals(nome, user.getNome());
-		
+
 		// Cria artigos
 		List<Artigo> artigos = new ArrayList<Artigo>();
 
